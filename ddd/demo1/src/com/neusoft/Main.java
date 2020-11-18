e0daf6c057f388e7736c309f04c807bf7c4d8bb1
dwl12316 <1906088619@qq.com>
2020-11-18 14:57:49 +0800

    Update Main.java

--- ddd/demo1/src/com/neusoft/Main.java
+++ ddd/demo1/src/com/neusoft/Main.java
@@ -3,7 +3,7 @@ package com.neusoft;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("111");
+        System.out.println("11");
 	// write your code here
     }
 }
